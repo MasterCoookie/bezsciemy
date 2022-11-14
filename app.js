27316537b73cd086cf20ea70b89af6815e205c61
MasterCoookie <jas.kocurek2@wp.pl>
2022-11-14 22:26:19 +0100

    basic app

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.listen(port, () => {
+    console.log("B E Z Ś C I E M Y listeining to requests on %s", port);
+});
+
+app.get('/', (req, res) => {
+    res.send("<h1>Witemy na B E Z Ś C I E M Y</h1>");
+});
