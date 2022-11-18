7b9be1852ac7d7e9d342bcf831b2cf32c2a9c002
MasterCoookie <jas.kocurek2@wp.pl>
2022-11-18 13:53:43 +0100

    middlewares added

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require('express');
+const session = require('express-session');
 
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(session({
+	secret: 'ZMiTAC',
+	resave: false,
+	//change last number to specify session max age in hours
+	cookie: { maxAge: 1000 * 3600 * 1 },
+	saveUninitialized: true
+}));
 
 const port = 3000;
 
