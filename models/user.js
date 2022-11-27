1e75f3ba5ba820afcc23dcffc63aeba96fc3ffad
macibol775 <macibol775@student.polsl.pl>
2022-11-27 17:17:15 +0100

    user extended

--- models/user.js
+++ models/user.js
@@ -1,20 +1,39 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-//const bcrypt = require('bcrypt');
+const { isEmail } = require('validator');
+const bcrypt = require('bcrypt');
+
+const pwdValid = (password) => {
+    if(/^\d+$/.test(password) || /^[a-zA-Z]+$/.test(password)) {
+        return false;
+    }
+    if(password.toUpperCase() === password || password.toLowerCase() === password) {
+        return false;
+    }
+    return true;
+}
 
 const userSchema = new Schema({
 	//todo: extend model
 	username: {
 		type: String,
-		required: true,
+		required: [true, 'Please enter a username'],
+		unique: [true, 'Username already exists'],
+		minlength: [3, 'Your username must be at least 3 characters long'],
+		maxlength: [24, 'Your username must be shorter than 24 characters'],
 	},
 	email: {
 		type: String,
-		required: true,
+		required: [true, 'Please enter an email'],
+		unique: [true, 'Email is already used by another user'],
+		validate: [isEmail, 'Please enter a valid email']
 	},
 	password: {
 		type: String,
-		required: true,
+		required: [true, 'Please enter a password'],
+		minlength: [8, 'Your password must be at least 3 characters long'],
+		maxlength: [24, 'Your password must be shorter than 24 characters'],
+		validate: [pwdValid, 'Your password must contain both letters (lowercase and uppercase) and numbers']
 	},
 });
 
@@ -22,9 +41,7 @@ userSchema.statics.login = async function (_username, _password) {
 	const user = await this.findOne({ username: _username });
 	if (user) {
 		//todo: add bcrypt
-		//console.log(user)
-		console.log(user.password);
-		console.log(_password);
+
 		if (user.password === _password) {
 			return user;
 		}
