059f00f9395478ad2cb24f8178793a992fa7ff10
MasterCoookie <jas.kocurek2@wp.pl>
2022-12-17 15:29:58 +0100

    post getting functional

--- controllers/contentListController.js
+++ controllers/contentListController.js
@@ -4,17 +4,21 @@ const Post = require('../models/postModel');
 const acquire_posts = async (query_type, page_number)  => {
     let posts;
     const paginate = page_number * 10;
-
-    if(query_type === "main") {
-        posts = await Post.find({ accepted_by: true }, { skip: paginate, limit: 10, sort: { accepted_at: 'desc' } });
+    try {
+        if(query_type === "main") {
+            posts = await Post.find({ accepted_by: { $exists: true } }).skip(paginate).limit(10).sort({ accepted_at: 'desc' });
+        }
+    } catch (err) {
+        console.log(err);
     }
+    
 
     return posts;
 }
 
 const main_get = async (req, res) => {
     //TODO page number
-    const posts = await acquire_posts("main", 1);
+    const posts = await acquire_posts("main", 0);
     console.log(posts);
 }
 
