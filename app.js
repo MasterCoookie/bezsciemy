a075d92f14a8a009ad3d42614e4ae3ad20b15ce2
MasterCoookie <jas.kocurek2@wp.pl>
2022-12-17 14:37:58 +0100

    router added to main

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const session = require('express-session');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/authRoutes');
 const postRoutes = require('./routes/postRoutes');
+const contentListRoutes = require('./routes/contentListRoutes');
 const secret = require('./secret');
 // const store = new session.MemoryStore();
 
@@ -47,9 +48,6 @@ app.use(
 	})
 );
 
+app.use('/', contentListRoutes);
 app.use('/auth', authRoutes);
 app.use('/post', postRoutes);
-
-app.get('/', (req, res) => {
-	res.send('<h1>Witamy na B E Z Ś C I E M Y</h1>');
-});
