287159a0ca0f96a4424d1321a70d46e2f6fa94ac
tomazaw <tomazaw886@student.polsl.pl>
2023-01-06 22:49:53 +0100

    Comment formula display after click

---
+++ public/scripts/comment-formula-script.js
@@ -0,0 +1,7 @@
+function comment() {
+	if (document.getElementById('comment-form').style.display === 'block') {
+		document.getElementById('comment-form').style.display = 'none';
+	} else {
+		document.getElementById('comment-form').style.display = 'block';
+	}
+}
