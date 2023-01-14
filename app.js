2629fd1f29a433c9184dd23e0dda14916403e341
MasterCoookie <jas.kocurek2@wp.pl>
2023-01-14 18:02:34 +0100

    app use public

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
-app.use(express.static('public'));
+app.use("*", express.static('public'));
 
 app.use(
 	session({
