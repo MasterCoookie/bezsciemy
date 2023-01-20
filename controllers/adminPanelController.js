717ef9dae1bf25b7094085d4e5570422035e8f35
MasterCoookie <jas.kocurek2@wp.pl>
2023-01-20 16:26:23 +0100

    wydmuszki made for admin panel

--- controllers/adminPanelController.js
+++ controllers/adminPanelController.js
@@ -2,6 +2,27 @@ const apply_get = (req, res) => {
 	res.render('/adminPanel/apply', { user: req.session.user, title: "Apply" })
 }
 
+const apply_post = (req, res) => {
+	res.sendStatus(501)
+}
+
+const review_post = (req, res) => {
+	res.sendStatus(501)
+}
+
+const list_get = (req, res) => {
+	res.render('/adminPanel/list', { user: req.session.user, title: "List" })
+}
+
+const review_get = (req, res) => {
+	const review_id = req.query.id;
+	res.render('/adminPanel/review', { user: req.session.user, title: "Review" })
+}
+
 module.exports = {
-	apply_get
+	apply_get,
+	apply_post,
+	list_get,
+	review_get,
+	review_post
 };
--- routes/adminPanelRoutes.js
+++ routes/adminPanelRoutes.js
@@ -7,6 +7,11 @@ const upload = multer();
 
 const router = express.Router();
 
-router.get('/apply', authController.login_get);
+router.get('/apply', authMiddleware.require_login, adminPanelController.apply_get);
+router.post('/apply', [upload.none(), authMiddleware.require_login], adminPanelController.apply_post);
+router.get('/review', authMiddleware.require_admin, adminPanelController.review_get);
+router.post('/review', authMiddleware.require_admin, adminPanelController.review_post);
+router.get('/', authMiddleware.require_admin, adminPanelController.list_get);
+
 
 module.exports = router;
