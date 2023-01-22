ed2281e6ab6bd6d7a0f50aeb9f7f1dd3ce9035ce
tomazaw <tomazaw886@student.polsl.pl>
2023-01-22 18:57:50 +0100

    Single application listing

--- controllers/adminPanelController.js
+++ controllers/adminPanelController.js
@@ -1,29 +1,28 @@
 const User = require('../models/userModel');
 
-const fill_aplications = async aplications => {
+const fill_applications = async (applications) => {
 	return await Promise.all(
-		aplications.map(async (aplication) => {
+		applications.map(async (application) => {
 			// uncomment after getting form db
 			// aplication = aplication.toObject()
-			let applier = await User.findById(aplication.applier_id);
-			applier = applier.toObject()
+			let applier = await User.findById(application.applier_id);
+			applier = applier.toObject();
 			if (applier) {
 				applier.password = undefined;
 				applier._id = undefined;
-				aplication.applier = applier;
+				application.applier = applier;
 			} else {
-				aplication.applier = 'deleted';
+				application.applier = 'deleted';
 			}
 
-			return aplication;
+			return application;
 		})
 	);
-}
+};
 
 const apply_get = (req, res) => {
-
-	res.render('adminPanel/apply', { user: req.session.user, title: "Apply" })
-}
+	res.render('adminPanel/apply', { user: req.session.user, title: 'Apply' });
+};
 
 const apply_post = (req, res) => {
 	res.sendStatus(501);
@@ -34,35 +33,45 @@ const review_post = (req, res) => {
 };
 
 //tmp dummy data
-const aplications = [
+const applications = [
 	{
-		_id: "6383ae11ffafdd02c0b8b56x",
-		aplication_perm_lvl: 2,
-		applier_id: "6383ae11ffafdd02c0b8b563",
-		content: "Admin dej redaktora pls, bede giga dobrym redaktorem i mam horom curke pls :((. btw sprzedam Opla"
+		_id: '6383ae11ffafdd02c0b8b56x',
+		application_perm_lvl: 2,
+		applier_id: '6383ae11ffafdd02c0b8b563',
+		content:
+			'Admin dej redaktora pls, bede giga dobrym redaktorem i mam horom curke pls :((. btw sprzedam Opla',
 	},
 	{
-		_id: "6383ae11ffafdd02c0b8b56z",
-		aplication_perm_lvl: 3,
-		applier_id: "6383af445323f013a20199ed",
-		content: "Admin dej admina pls, bede giga dobrym adminem i mam horego synka Fifonża :(( a dotego mnie wykastrowali, więc sobie nie zrobię kolejnego :(("
+		_id: '6383ae11ffafdd02c0b8b56z',
+		application_perm_lvl: 3,
+		applier_id: '6383af445323f013a20199ed',
+		content:
+			'Admin dej admina pls, bede giga dobrym adminem i mam horego synka Fifonża :(( a dotego mnie wykastrowali, więc sobie nie zrobię kolejnego :((',
 	},
 ];
 
 const list_get = async (req, res) => {
-	const aplications_filled = await fill_aplications(aplications);
-	
-	console.log(aplications_filled);
-	res.render('adminPanel/list', { user: req.session.user, title: "List", aplications: aplications_filled })
-}
+	const applications_filled = await fill_applications(applications);
+
+	console.log(applications_filled);
+	res.render('adminPanel/list', {
+		user: req.session.user,
+		title: 'List',
+		applications: applications_filled,
+	});
+};
 
 const review_get = async (req, res) => {
 	const review_id = req.query.id;
 
-	const aplications_filled = await fill_aplications(aplications);
+	const applications_filled = await fill_applications(applications);
 
-	res.render('adminPanel/review', { user: req.session.user, title: "Review", aplication: aplications_filled[0] })
-}
+	res.render('adminPanel/review', {
+		user: req.session.user,
+		title: 'Review',
+		application: applications_filled[0],
+	});
+};
 
 module.exports = {
 	apply_get,
