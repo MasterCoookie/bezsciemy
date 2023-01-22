0b1e3e030e327eeb5585bf4d5662ed9edabaec9d
MasterCoookie <jas.kocurek2@wp.pl>
2023-01-22 18:07:43 +0100

    dummy data

--- controllers/adminPanelController.js
+++ controllers/adminPanelController.js
@@ -11,13 +11,31 @@ const review_post = (req, res) => {
 	res.sendStatus(501);
 };
 
+//tmp dummy data
+const aplications = [
+	{
+		_id: "6383ae11ffafdd02c0b8b56x",
+		aplication_perm_lvl: 2,
+		applier_id: "6383ae11ffafdd02c0b8b563",
+		content: "Admin dej redaktora pls, bede giga dobrym redaktorem i mam horom curke pls :((. btw sprzedam Opla"
+	},
+	{
+		_id: "6383ae11ffafdd02c0b8b56z",
+		aplication_perm_lvl: 3,
+		applier_id: "6383af445323f013a20199ed",
+		content: "Admin dej admina pls, bede giga dobrym adminem i mam horego synka Fifonża :(( a dotego mnie wykastrowali, więc sobie nie zrobię kolejnego :(("
+	},
+];
+
 const list_get = (req, res) => {
-	res.render('adminPanel/list', { user: req.session.user, title: "List" })
+	
+	
+	res.render('adminPanel/list', { user: req.session.user, title: "List", aplications })
 }
 
 const review_get = (req, res) => {
 	const review_id = req.query.id;
-	res.render('adminPanel/review', { user: req.session.user, title: "Review" })
+	res.render('adminPanel/review', { user: req.session.user, title: "Review", aplication: aplications[0] })
 }
 
 module.exports = {
