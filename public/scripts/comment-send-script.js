8925b46bcbfc1f6d0464789c6c1a3dc1242c7467
tomazaw <tomazaw886@student.polsl.pl>
2023-01-26 22:42:28 +0100

    code formatting

--- public/scripts/comment-send-script.js
+++ public/scripts/comment-send-script.js
@@ -1,10 +1,10 @@
 function sendComment(postID, event = 1) {
 	let commentForm;
 	if(event !== 1) {
-	commentForm = document.getElementById(event.target.form.id);
+		commentForm = document.getElementById(event.target.form.id);
 	}
 	else {
-	commentForm = document.getElementById("comment-form");
+		commentForm = document.getElementById("comment-form");
 	}
 	const formData = new FormData(commentForm);
 	formData.append('postID', postID);
@@ -16,13 +16,12 @@ function sendComment(postID, event = 1) {
 		request.addEventListener('load', (serverResponse) => {
 			const response = JSON.parse(serverResponse.target.responseText);
 			if(event === 1) {
-			document.getElementById('comment-message').innerHTML = response.msg;
+				document.getElementById('comment-message').innerHTML = response.msg;
 			}
 			else {
-			document.getElementById('comment-message-' + event.target.form.id).innerHTML = response.msg;
+				document.getElementById('comment-message-' + event.target.form.id).innerHTML = response.msg;
 			}
 		});
-		
 		request.open('PUT', '/post/comment');
 		request.send(formData);
 	}
