f1cd78006e4826500f2a075effaab066f835a634
macibol775 <macibol775@student.polsl.pl>
2023-01-26 21:55:08 +0100

    added application model

---
+++ models/applicationModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const applicationSchema = new Schema({
+    applier_id: {
+        type: Schema.Types.ObjectId,
+		required: true
+    },
+    application_perm_lvl: {
+        type: Number,
+		required: true
+    },
+    content: {
+        type: String,
+		required: true
+    },
+    approved_by: Schema.Types.ObjectId,
+    approved_at: Date
+});
+
+applicationSchema.methods.approveApplicationAndSave = async function(_user){
+    this.approved_by = _user;
+    this.approved_at = new Date();
+    await this.save();
+}
+
+const Application = mongoose.model('Application', applicationSchema);
+
+module.exports = Application;
