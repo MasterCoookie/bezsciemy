c9984b47bf87519851dad61532d049addfcc741f
tomazaw <tomazaw886@student.polsl.pl>
2023-02-06 15:00:20 +0100

    bugfixes and tweaks

--- public/scripts/comment-send-script.js
+++ public/scripts/comment-send-script.js
@@ -15,12 +15,8 @@ function sendComment(postID, event = 1) {
 	if(commentForm.content.length !== 0) {
 		request.addEventListener('load', (serverResponse) => {
 			const response = JSON.parse(serverResponse.target.responseText);
-			if(event === 1) {
 				document.getElementById('comment-message').innerHTML = response.msg;
-			}
-			else {
-				document.getElementById('comment-message-' + event.target.form.id).innerHTML = response.msg;
-			}
+				location.reload();
 		});
 		request.open('PUT', '/post/comment');
 		request.send(formData);
